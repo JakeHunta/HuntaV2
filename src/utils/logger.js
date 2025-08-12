b5772096d4d6b4f1abf971bce21e8a6b3e32706f
JakeHunta <jake@hunta.uk>
2025-08-12 16:15:48 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,13 +1,5 @@
-const morgan = require('morgan');
-
-const jsonFormat = (tokens, req, res) => JSON.stringify({
-  method: tokens.method(req, res),
-  url: tokens.url(req, res),
-  status: Number(tokens.status(req, res)),
-  contentLength: tokens.res(req, res, 'content-length'),
-  responseTimeMs: Number(tokens['response-time'](req, res))
-});
-
-module.exports = {
-  httpLogger: morgan(jsonFormat)
+export const logger = {
+  info:  (...a) => console.log(...a),
+  warn:  (...a) => console.warn(...a),
+  error: (...a) => console.error(...a),
 };
