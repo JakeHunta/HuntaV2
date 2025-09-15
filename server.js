48d7986964e726e4a2353029c45963ee77c8da31
JakeHunta <jake@hunta.uk>
2025-09-15 16:38:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,15 @@ app.use((req, res, next) => {
   next();
 });
 
+// Global CORS middleware to ensure all responses have CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // Basic global rate limit
 app.use(
   rateLimit({
