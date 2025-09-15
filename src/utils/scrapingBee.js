69acf7b598c227c61cea89e7b350366f109922d2
JakeHunta <jake@hunta.uk>
2025-09-15 11:02:16 +0100

    Update scrapingBee.js

--- src/utils/scrapingBee.js
+++ src/utils/scrapingBee.js
@@ -1,9 +1,5 @@
-// Robust ScrapingBee wrapper
-// Ensures target URL is always passed, adds helpful upstream debug,
-// and sets sane defaults that work well for eBay/FB/Gumtree.
-
+// Hardened ScrapingBee wrapper: prevents base 404s and adds sane defaults
 import axios from "axios";
-
 const BEE_BASE = "https://app.scrapingbee.com/api/v1/";
 
 export async function scrapingBee({
@@ -15,7 +11,7 @@ export async function scrapingBee({
   country_code = "gb",
   headers = {},
   json_response = false,
-  timeout = 20000,
+  timeout = 20000
 }) {
   if (!url) throw new Error("ScrapingBee missing target url");
 
@@ -25,28 +21,15 @@ export async function scrapingBee({
     render_js: render_js ? "true" : "false",
     premium_proxy: premium_proxy ? "true" : "false",
     block_resources: block_resources ? "true" : "false",
-    country_code,
+    country_code
   };
   if (wait) params.wait = String(wait);
   if (json_response) params.json_response = "true";
 
-  try {
-    const res = await axios.get(BEE_BASE, {
-      params,
-      headers,
-      timeout,
-    });
-    return res.data;
-  } catch (err) {
-    const beeStatus = err?.response?.status;
-    const data = err?.response?.data;
-    if (json_response && data) {
-      console.warn("ScrapingBee upstream:", {
-        beeStatus,
-        original_status: data.original_status,
-        original_status_text: data.original_status_text,
-      });
-    }
+  const res = await axios.get(BEE_BASE, { params, headers, timeout });
+  return res.data;
+}
+
     throw err;
   }
 }
